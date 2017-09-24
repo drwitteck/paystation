e7e248a75aa540efc1f311e756828111c31ab23f
Derek Witteck <drwitteck@gmail.com>
2017-09-24 19:37:41 -0400

    Finished progressive rate class and tests.

--- src/paystation/domain/ProgressiveRateStrategy.java
+++ src/paystation/domain/ProgressiveRateStrategy.java
@@ -3,6 +3,18 @@ package paystation.domain;
 public class ProgressiveRateStrategy implements RateStrategy {
     @Override
     public int parkingRateCalculation(int totalAmountEntered) {
-        return totalAmountEntered * 2 / 5;
+        int time = 0;
+
+        if (totalAmountEntered <= 150) {
+            time = totalAmountEntered * 2 / 5;
+        }
+        else if (totalAmountEntered > 150 && totalAmountEntered <= 350){
+            time = 60 + (totalAmountEntered - 150) * 3 / 10;
+        }
+        else if (totalAmountEntered > 350){
+            time = 120 + (totalAmountEntered - 350) * 2 / 10;
+        }
+
+        return time;
     }
 }
--- test/paystation/domain/ProgressiveRateStrategyTest.java
+++ test/paystation/domain/ProgressiveRateStrategyTest.java
@@ -49,7 +49,7 @@ public class ProgressiveRateStrategyTest {
      * Entering $3.50 should make the display report 120 minutes of parking time
      */
     @Test
-    public void shouldDisplay120MinFor300cents()
+    public void shouldDisplay120MinFor350cents()
         throws IllegalCoinException{
         addDollar();
         addDollar();
@@ -57,6 +57,23 @@ public class ProgressiveRateStrategyTest {
         addHalfDollar();
 
         assertEquals("Should display 120 minutes for 350 cents", 120, ps.readDisplay());
+    }
+
+    @Test
+    /**
+     * Entering $6.50 should make the display report 180 minutes of parking time
+     */
+    public void shouldDisplay180MinFor650cents()
+        throws IllegalCoinException{
+        addDollar();
+        addDollar();
+        addDollar();
+        addDollar();
+        addDollar();
+        addDollar();
+        addHalfDollar();
+
+        assertEquals("Should display 180 minutes for 650 cents", 180, ps.readDisplay());
     }
 
 }
