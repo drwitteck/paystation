2cc7366d7fa62271a427b7eef43926e669370ba3
Derek Witteck <drwitteck@gmail.com>
2017-09-24 23:08:20 -0400

    Added Linear Rate Strategy test.

---
+++ test/paystation/domain/LinearRateStrategyTest.java
@@ -0,0 +1,29 @@
+package paystation.domain;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LinearRateStrategyTest {
+    RateStrategy rs;
+
+    @Before
+    public void setUp() throws Exception {
+        rs = new LinearRateStrategy();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void shouldDisplay120MinFor300cents()
+        throws IllegalCoinException{
+        assertEquals(300 / 5 * 2, rs.parkingRateCalculation(300));
+
+    }
+
+
+}
